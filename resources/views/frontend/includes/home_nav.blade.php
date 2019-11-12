c8c448cb30f82c23dcbb851802c709e374240068
smith199 <charlesbasoah93@gmail.com>
2019-11-12 12:11:16 +0000

    three

---
+++ resources/views/frontend/includes/home_nav.blade.php
@@ -0,0 +1,16 @@
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark shadow-sm fixed-top">
+    <a class="navbar-brand text-white navbrand" href="{{route('pages.welcome')}}">
+    CUCG STUDENT PORTAL
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+      aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+      <ul class="navbar-nav mr-auto">
+      </ul>
+      <ul class="navbar-nav ml-auto nav-flex-icons">
+
+      </ul>
+    </div>
+  </nav>
