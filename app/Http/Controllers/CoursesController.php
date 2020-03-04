3435d25d90d4be98a89dbac81dde7ebf3aa87cff
gods-i <Kofibaidoo1993@gmail.com>
2020-03-04 18:57:14 +0000

    thu1

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\AcademicYear;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Course;
+use  App\Program;
+use  App\SemesterRegcourse;
+use App\ProgramOption;
+use Illuminate\Support\Facades\DB;
+
+
 
 
 class CoursesController extends Controller
 {
+    protected $semester_reg_course, $program, $program_option, $academic_year;
+
+    public function __construct()
+    {
+        $this->semester_reg_course = new SemesterRegcourse();
+        $this->program = new Program();
+        $this->program_option = new ProgramOption();
+        $this->academic_year = new AcademicYear();
+
+    }
+
     #IMPORT CSV FILE INTO DATABASE
     public function importFile()
     {
@@ -57,6 +75,75 @@ class CoursesController extends Controller
 
     public function processSemesterCourses(Request $request)
     {
+
+            $this->validate($request, [
+            'course_name'        =>     'required',
+            'semester'           =>     'required|min:8|max:100',
+            'level'              =>     'required|min:3|max:3',
+            'academic_year'      =>     'required',
+            'programme'          =>     'required',
+            'programme_option'   =>     'required',
+            'admission_type'     =>     'required',
+            'stream'             =>     'required'
+        ]);
+
+        DB::beginTransaction();
+        try
+        {
+
+            $semester_reg_course = $this->semester_reg_course->create([
+                'course_name'       =>      $request['course_name'],
+                'semester'          =>      $request['semester'],
+                'level'             =>      $request['level'],
+                'admission_type'    =>      $request['admission_type'],
+                'stream'            =>      $request['stream']
+
+
+
+
+
+
+
+
+                // 'course_name'       =>      $request->course_name,
+                // 'semester'          =>      $request->semester,
+                // 'level'             =>      $request->level,
+                // 'admission_type'    =>      $request->admission_type,
+                // 'stream'            =>      $request->stream,
+
+            ]);
+
+
+            $program_option = $this->program_option->create([
+                'Option_name'       =>      $request['programme_option'],
+
+
+            ]);
+
+            $program = $this->program->create([
+            'program_name'          =>      $request['programme'],
+            ]);
+
+
+            $academic_year = $this->academic_year->create([
+            'academic_year'         =>      $request['academicyear'],
+
+
+            ]);
+
+            if ($semester_reg_course && $program_option && $program && $academic_year)
+            {
+                DB::commit();
+            }
+            else{
+                DB::rollBack();
+            }
+            return redirect()->back();
+        }
+        catch (\Exception $exception) {
+            DB::rollBack();
+
+        }
 
     }
 }
--- resources/views/backend/courses/add_semester_courses.blade.php
+++ resources/views/backend/courses/add_semester_courses.blade.php
@@ -30,7 +30,7 @@
                                 <option value="Second Semester" name="semester">Second Semester</option>
                             </select>
                         </div>
-                        
+
                         <div class="col-md-4 mb-3">
                             <label class="text-uppercase" for="week">PROGRAMME</label>
                             <select class="custom-select" id="inputGroupSelect01" name="programme">
@@ -90,12 +90,12 @@
                         </div>
 
                         <div class="col-md-8 mb-3">
-                            <label class="text-uppercase" for="activity">COURSE NAME</label>
+                            <label class="text-uppercase" for="course_name">COURSE NAME</label>
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="fa fa-book"></i></span>
                                 </div>
-                                <textarea class="form-control" name="activity" aria-label="With textarea"></textarea>
+                                <textarea class="form-control" name="course_name" aria-label="With textarea"></textarea>
                             </div>
                         </div>
                     </div>
